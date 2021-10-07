b3662859fdf2ea5faa64bab450bc713ba2ef63f5
NISHI-SPEC <80394806+NISHI-SPEC@users.noreply.github.com>
2021-10-07 20:39:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+function preload() {
+}
+
+function setup() {
+  canvas = createCanvas(640, 480);
+    canvas.position(110, 250);
+    video = createCapture(VIDEO);
+    video.hide();
+  background(300);
+    noFill();
+beginShape();
+vertex(50, 20);
+vertex(600, 20);
+vertex(600, 450);
+vertex(50, 450);
+endShape(CLOSE);
+  fill(255, 0, 0,);
+  stroke(127, 63, 120);
+circle(50, 30, 50);
+ circle(50, 450, 50);
+  circle(600, 30, 50);
+   circle(600, 450, 50);
+}
+
+function draw() {
+    image(video, 0, 0, 640, 480);
+    tint(tint_color);
+}
+
+function take_snapshot(){
+save('student_name.png');
+}
+
+function draw() {
+    image(video, 0, 0, 640, 480);
+    tint(tint_color);
+}
+
+function take_snapshot(){
+save('student_name.png');
+}
